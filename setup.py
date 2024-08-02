f6fa563e28f3fca267e484c53886fc2938b15474
AlejoPrietoDavalos <alejoprietodavalos@gmail.com>
2024-08-02 12:31:17 -0300

    0.0.1 -> 0.0.2: Se crea el objeto Airtable y el paquete operations con las funcionalidades básicas de airtable.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name = "airtable_pack",
-    version = "0.0.1",
+    version = "0.0.2",
     author = "Andrea Soledad Guerra",
     packages = find_packages(),
     description = "Python wrapper for Airtable Pack.",

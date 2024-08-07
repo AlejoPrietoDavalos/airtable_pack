227a5c47ab87cacfe0870cd71b70642bbed57318
andreasoledadguerra <andrea.guerra@outlook.com.ar>
2024-08-06 22:39:21 -0300

    Se actualiza la versión 0.0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name = "airtable_pack",
-    version = "0.0.2",
+    version = "0.0.3",
     author = "Andrea Soledad Guerra",
     packages = find_packages(),
     description = "Python wrapper for Airtable Pack.",
